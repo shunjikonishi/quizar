f7dcb399381e6fcbb13080d345127835cff49941
k-shunji <k-shunji@flect.co.jp>
2014-02-25 16:17:25 +0900

    Add tablesorter

--- public/javascripts/design.js
+++ public/javascripts/design.js
@@ -1,4 +1,33 @@
 $(function() {
+	var sorted = false;
+	function randomSortRanking() {
+		if (sorted) {
+			return;
+		}
+		var $table = $("#tbl-ranking-current"),
+			tds = [];
+		$table.find("tbody tr").each(function(index, el) {
+			var $td = $(this).find("td:first"),
+				num = Math.random();
+			tds.push({
+				"td" : $td,
+				"num" : num
+			});
+		});
+		tds.sort(function(a, b) {
+			return a.num < b.num ? -1 : 1;
+		})
+		for (var i=0; i<tds.length; i++) {
+			tds[i].td.text(i+1);
+		}
+		setTimeout(function() {
+			$table.tableSort({
+				"sortBy" : ["numeric", "nosort", "nosort", "nosort"]
+			});
+			$table.find("thead th:first").click();
+		}, 100);
+		sorted = true;
+	}
 	$("#sidemenu").sidr({
 		"onOpen" : function() {
 			$("#header").css("left", "260px").find(".sidemenu-collapse").hide();
@@ -15,5 +44,23 @@ $(function() {
 			$.sidr('open');
 		},
 		"preventDefaultEvents": false
-	});
+	})
+	$("#content > div").hide();
+	$("#content > div:first-child").show();
+	$("#sidr a").click(function() {
+		var id = $(this).attr("href");
+		$("#content > div").hide();
+		$.sidr("close", function() {
+			$(id).show("slide", { "direction" : "right"}, 750, function() {
+				if (id == "#ranking") {
+					randomSortRanking();
+				}
+			});
+		});
+	})
+	$("#tab-ranking a").on("shown.bs.tab", function(e) {
+		if ($(e.target).attr("href") == "#ranking-current") {
+			randomSortRanking();
+		}
+	})
 })
